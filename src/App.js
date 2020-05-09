b58e25b624581e991172e7795a48359ab0367bc3
kkznch <kkznch710md@gmail.com>
2020-05-09 15:08:43 +0900

    [update]ベースとなるコンポーネントを作った

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+import Workspace from './components/pages/Workspace';
+import TopNavigation from './components/molecules/TopNavigation';
 
-function App() {
+const headerHeightRem = 2.4;
+const sidebarWidthRem = 3.6;
+
+const App = styled.div`
+display: flex;
+flex-direction: column;
+height: 100vh;
+width: 100vw;
+`;
+
+const Header = styled.div`
+height: ${headerHeightRem}rem;
+width: 100%;
+background-color: red;
+`;
+
+const Wrapper = styled.div`
+display: flex;
+flex-direction: row;
+height: calc(100% - ${headerHeightRem}rem);
+width: 100%;
+background-color: blue;
+`;
+
+const Sidebar = styled.div`
+height: 100%;
+width: ${sidebarWidthRem}rem;
+background-color: pink;
+`;
+
+const Content = styled.div`
+height: 100%;
+width: calc(100% - ${sidebarWidthRem}rem);
+background-color: yellow;
+`;
+
+export default () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <App>
+      <Header>
+        <TopNavigation></TopNavigation>
+      </Header>
+      <Wrapper>
+        <Sidebar></Sidebar>
+        <Content>
+          <Workspace></Workspace>
+        </Content>
+      </Wrapper>
+    </App>
   );
 }
-
-export default App;
